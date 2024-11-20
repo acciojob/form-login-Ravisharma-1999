df6848aca30c57a665b422a67dd6b0ace7b3b9ce
Ravisharma-1999 <ravi829144@gmail.com>
2024-11-20 11:44:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-function getFormvalue() {
-    //Write your code here
+function getFormvalue(event) {
+  // Prevent the form from submitting and refreshing the page
+  event.preventDefault();
 
+  // Get the first name and last name values from the form
+  const firstName = document.forms["form1"]["fname"].value;
+  const lastName = document.forms["form1"]["lname"].value;
+
+  // Combine first name and last name and display it in an alert
+  alert(firstName + " " + lastName);
 }
